ce2062fe5367a3cb97af851a7515f205932bc91c
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-03-29 20:59:20 +0530

    Added Stock Client

--- src/client/stock.py
+++ src/client/stock.py
@@ -4,7 +4,7 @@ from starlette.exceptions import HTTPException
 from src.schemas.investment import StockResponse
 
 
-class stockClient():
+class stockClient:
     @classmethod
     def get_stock_data(cls, symbol: str) -> StockResponse:
         data = yf.Ticker(symbol).info
@@ -14,13 +14,16 @@ class stockClient():
             data = yf.Ticker(symbol + ".BO").info
         try:
             return StockResponse(
-                name=data['longName'],
-                currency=data['financialCurrency'] if 'financialCurrency' in data else data['currency'],
-                price=data['open'],
-                symbol=data['symbol'],
+                name=data["longName"],
+                currency=data["financialCurrency"]
+                if "financialCurrency" in data
+                else data["currency"],
+                price=data["open"],
+                symbol=data["symbol"],
             )
         except:
             raise HTTPException(status_code=404, detail="Stock not found")
+
 
 if __name__ == "__main__":
     print(stockClient.get_stock_data("ADANIENT"))
