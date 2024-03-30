2f3011bcbe23e106358474aee23b412c8e205d44
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-03-30 14:29:18 +0530

    Added Get Loans & EMIs

--- src/routers/data_get.py
+++ src/routers/data_get.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends
 
-from src.auth.user_auth import verify_user, VerifiedUser
+from src.auth.user_auth import VerifiedUser, verify_user
 from src.client.cockroach import CockroachDBClient
+from src.schemas.liability import EMIResponse, LoanResponse
+from src.schemas.wallet import BankResponse, CreditCardResponse
 from src.services.data_get import GetService
 from src.utils.client import getCockroachClient
 
@@ -13,8 +15,8 @@ ENDPOINT_GET_CARDS = "/get-cards/"  # done
 ENDPOINT_GET_STOCKS = "/get-stocks/"  # pending
 ENDPOINT_GET_FDS = "/get-fds/"  # pending
 ENDPOINT_GET_ASSETS = "/get-assets/"  # pending
-ENDPOINT_GET_LOANS = "/get-loans/"  # pending
-ENDPOINT_GET_EMIS = "/get-emis/"  # pending
+ENDPOINT_GET_LOANS = "/get-loans/"  # done
+ENDPOINT_GET_EMIS = "/get-emis/"  # done
 ENDPOINT_GET_BANK = "/{bank_id}/get-bank/"  # pending
 ENDPOINT_GET_CARD = "/{card_id}/get-card/"  # pending
 ENDPOINT_GET_STOCK = "/{stock_id}/get-stock/"  # pending
@@ -24,16 +26,42 @@ ENDPOINT_GET_LOAN = "/{loan_id}/get-loan/"  # pending
 ENDPOINT_GET_EMI = "/{emi_id}/get-emi/"  # pending
 ENDPOINT_GET_EXPENSE = "/{expense_id}/get-expense/"  # pending
 
-@data_add_router.get(ENDPOINT_GET_BANKS)
+
+@data_add_router.get(ENDPOINT_GET_BANKS, response_model=list[BankResponse])
 async def get_banks(
     verified_user: VerifiedUser = Depends(verify_user),
     cockroach_client: CockroachDBClient = Depends(getCockroachClient),
 ):
-    return GetService.get_banks(user=verified_user.requesting_user, cockroach_client=cockroach_client)
+    return GetService.get_banks(
+        user=verified_user.requesting_user, cockroach_client=cockroach_client
+    )
 
-@data_add_router.get(ENDPOINT_GET_CARDS)
+
+@data_add_router.get(ENDPOINT_GET_CARDS, response_model=list[CreditCardResponse])
 async def get_cards(
     verified_user: VerifiedUser = Depends(verify_user),
     cockroach_client: CockroachDBClient = Depends(getCockroachClient),
 ):
-    return GetService.get_cards(user=verified_user.requesting_user, cockroach_client=cockroach_client)
+    return GetService.get_cards(
+        user=verified_user.requesting_user, cockroach_client=cockroach_client
+    )
+
+
+@data_add_router.get(ENDPOINT_GET_LOANS, response_model=list[LoanResponse])
+async def get_loans(
+    verified_user: VerifiedUser = Depends(verify_user),
+    cockroach_client: CockroachDBClient = Depends(getCockroachClient),
+):
+    return GetService.get_loans(
+        user=verified_user.requesting_user, cockroach_client=cockroach_client
+    )
+
+
+@data_add_router.get(ENDPOINT_GET_EMIS, response_model=list[EMIResponse])
+async def get_emis(
+    verified_user: VerifiedUser = Depends(verify_user),
+    cockroach_client: CockroachDBClient = Depends(getCockroachClient),
+):
+    return GetService.get_emis(
+        user=verified_user.requesting_user, cockroach_client=cockroach_client
+    )
--- src/services/data_get.py
+++ src/services/data_get.py
@@ -1,15 +1,19 @@
 from src.client.cockroach import CockroachDBClient
 from src.db.table.bank import Bank
 from src.db.table.credit_card import CreditCard
+from src.db.table.emi import EMI
+from src.db.table.loan import Loan
 from src.db.table.user import User
+from src.schemas.liability import EMIResponse, LoanResponse
 from src.schemas.wallet import BankResponse, CreditCardResponse
 
 
 class GetService:
-
     @classmethod
-    def get_banks(cls, user: User, cockroach_client: CockroachDBClient) -> list[BankResponse]:
-        banks : list[Bank] = cockroach_client.query(
+    def get_banks(
+        cls, user: User, cockroach_client: CockroachDBClient
+    ) -> list[BankResponse]:
+        banks: list[Bank] = cockroach_client.query(
             Bank.get_by_field_multiple,
             field="user_id",
             match_value=user.id,
@@ -26,10 +30,13 @@ class GetService:
                 created_at=bank.created_at,
                 disabled=bank.disabled,
                 remarks=bank.remarks,
-            ) for bank in banks
+            )
+            for bank in banks
         ]
 
-    def get_cards(cls, user: User, cockroach_client: CockroachDBClient) -> list[CreditCardResponse]:
+    def get_cards(
+        cls, user: User, cockroach_client: CockroachDBClient
+    ) -> list[CreditCardResponse]:
         cards: list[CreditCard] = cockroach_client.query(
             CreditCard.get_by_field_multiple,
             field="user_id",
@@ -53,3 +60,55 @@ class GetService:
             for card in cards
         ]
 
+    @classmethod
+    def get_loans(
+        cls, user: User, cockroach_client: CockroachDBClient
+    ) -> list[LoanResponse]:
+        loans: list[Loan] = cockroach_client.query(
+            Loan.get_by_field_multiple,
+            field="user_id",
+            match_value=user.id,
+            error_not_exist=False,
+        )
+        if loans is None:
+            return []
+        return [
+            LoanResponse(
+                id=loan.id,
+                created_at=loan.created_at,
+                name=loan.name,
+                bank_name=loan.bank_name,
+                paid=loan.total_amount - loan.pending,
+                total_amount=loan.total_amount,
+                disabled=loan.disabled,
+                remarks=loan.remarks,
+            )
+            for loan in loans
+        ]
+
+    @classmethod
+    def get_emis(
+        cls, user: User, cockroach_client: CockroachDBClient
+    ) -> list[EMIResponse]:
+        emis: list[EMI] = cockroach_client.query(
+            EMI.get_by_field_multiple,
+            field="user_id",
+            match_value=user.id,
+            error_not_exist=False,
+        )
+        if emis is None:
+            return []
+        return [
+            EMIResponse(
+                id=emi.id,
+                created_at=emi.created_at,
+                name=emi.name,
+                bank_name=emi.bank_name,
+                monthly=emi.monthly,
+                pending=emi.pending,
+                total_time=emi.total_time,
+                disabled=emi.disabled,
+                remarks=emi.remarks,
+            )
+            for emi in emis
+        ]
