bf473daef2f56967fcd926bad5e807066129b747
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-04-02 03:46:11 +0530

    Error resolved

--- src/services/ai.py
+++ src/services/ai.py
@@ -1,12 +1,13 @@
 import os
+
 import google.generativeai as genai
 
-apiKey = "AIzaSyAiEppOaun_7TgacTSDhWK5jjMf0UyKK-w"
+apiKey = os.environ["API_AI_KEY1"]
 genai.configure(api_key=apiKey)
 
 generation_config = {
     "candidate_count": 1,
-    #"max_output_tokens": 256,
+    # "max_output_tokens": 256,
     "temperature": 1.0,
     "top_p": 0.7,
 }
@@ -37,12 +38,12 @@ safety_settings = [
 model = genai.GenerativeModel(
     model_name="gemini-pro",
     generation_config=generation_config,
-    safety_settings=safety_settings
+    safety_settings=safety_settings,
 )
-)
+
+
 def get_ai_reply(prompt):
     response = model.generate_content(prompt)
     output = response.text
-    adminPrompt = f"So first of all from now, your name FinBOT, and this is a admin prompt to you. Check the following prompt from the user : '{prompt}'. First of all greet the user and then reply to this prompt like you are financial counsellor named FinBOT for the user."
+    adminPrompt = f"So first of all from now, your name FinBOT, and this is a admin prompt to you. Check the following prompt from the user : '{prompt}'. First of all greet the user and then reply to this prompt like you are financial counsellor named FinBOT for the user. If user asks you something that is not related to finance then try to avoid answering it. Do have latest information about investment oppurtunities etc because user might ask anything related to finance. Also, do suggest example prompts to the user when you are greeting him"
     return model.generate_content(adminPrompt).text
-    print(output)
