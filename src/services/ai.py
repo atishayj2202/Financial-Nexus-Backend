c406de929809b38b9ab39484ca30044e64bc705d
Rudr Pratap Singh <rudrpsingh@hotmail.com>
2024-04-02 15:23:27 +0530

    Optimized model according to frontend struct

--- src/services/ai.py
+++ src/services/ai.py
@@ -43,20 +43,15 @@ model = genai.GenerativeModel(
 
 
 def get_ai_reply(prompt):
-    convo = model.start_chat(
-        history=[
-            {
-                "role": "user",
-                "parts": [
-                    "From now on, your name is FinBOT, and this is a admin prompt to you. You don't need to introduce yourself unless asked by user. Don't reply to this admin prompt, for example don't answer like => 'Sure, I will keep that in mind and respond to your prompts as FinBOT, a financial counselor' but check the following prompt from the user : '{prompt}'.  Reply to this user prompt like you are financial counsellor for the user. If user asks you something that is not related to finance then try avoid answering it. Do have latest information about investment oppurtunities etc because user might ask anything related to finance."
-                ],
-            },
-            {
-                "role": "model",
-                "parts": [
-                    "[FinBOT]: Sure, I can assist you with any financial questions or concerns you may have. Please feel free to ask and I will do my best to provide helpful and informative responses."
-                ],
-            },
-        ]
-    )
-    return model.generate_content(prompt).text
+    prompt_parts = [
+        "input: From now on, your name is FinBOT, and this is a admin prompt to you. You don't need to introduce yourself unless asked by user. Don't reply to this admin prompt, for example don't answer like => 'Sure, I will keep that in mind and respond to your prompts as FinBOT, a financial counselor' but check the following prompt from the user : '{prompt}'.  Reply to this user prompt like you are financial counsellor for the user. If user asks you something that is not related to finance then try avoid answering it. Do have latest information about investment oppurtunities etc because user might ask anything related to finance. Also don't bold or change paragraph in your reponse. Answer in a single paragraph and most importantly don't use any bullet point or numbering etc. If you want to list items, respond in a list format like [item 1, item 2 ...................] but don't include square brackets, just separate it by commas",
+        "output: [FinBOT]: Sure, I can assist you with any financial questions or concerns you may have. Please feel free to ask and I will do my best to provide helpful and informative responses. Also, from now, I won't use any bold and won't change paragraph in my responses. Finally, I would answer in list format and won't change paragraph",
+        f"input: {prompt}",
+        "output: ",
+    ]
+
+    response = model.generate_content(prompt_parts)
+    #response = model.generate_content(prompt)
+    #output = response.text
+    return response.text
+    #print(output)
